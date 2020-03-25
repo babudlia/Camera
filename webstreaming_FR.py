cd32356d4e1ad4460b178a0ce8d6c84b5a2360c4
babudlia <48296845+babudlia@users.noreply.github.com>
2020-03-25 12:55:00 +0100

    Update webstreaming_FR.py

--- webstreaming_FR.py
+++ webstreaming_FR.py
@@ -33,24 +33,19 @@ lock = threading.Lock()
 # initialize a flask object
 app = Flask(__name__)
 
-def streamer_pipeline(
-	capture_width=1280,
-	capture_height=720,
-	display_width=1280,
-	display_height=720,
-	framerate=60,
-	flip_method=0,
-):
-	return (
-		%(
-			capture_width,
-			capture_height,
-			framerate,
-			flip_method,
-			display_width,
-			display_height,
-		)
-	)
+def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=60, flip_method=0):
+    """
+    Return an OpenCV-compatible video source description that uses gstreamer to capture video from the camera on a Jetson Nano
+    """
+    return (
+            f'nvarguscamerasrc ! video/x-raw(memory:NVMM), ' +
+            f'width=(int){capture_width}, height=(int){capture_height}, ' +
+            f'format=(string)NV12, framerate=(fraction){framerate}/1 ! ' +
+            f'nvvidconv flip-method={flip_method} ! ' +
+            f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
+            'videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+            )
+
 def gstreamer_pipeline(
     capture_width=1280,
     capture_height=720,
@@ -82,7 +77,7 @@ def gstreamer_pipeline(
 #vs = VideoStream(usePiCamera=1).start()
 #vs = VideoStream(src=0).start()
 #vs = VideoStream(src=gstreamer_pipeline(flip_method=0)).start()
-vs = VideoStream(src=streamer_pipeline(flip_method=0)).start()
+vs = VideoStream(src=get_jetson_gstreamer_source().start()
 time.sleep(0.0)
 
 @app.route("/")
